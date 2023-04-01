f993a9b6c65ac2f0a2a828b2776570aba48fe443
ajnares <77725310+ajnares@users.noreply.github.com>
2023-04-01 16:33:39 +0800

    base website. FLASK

---
+++ design/static/index.js
@@ -0,0 +1,38 @@
+const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "#ffffff",
+        borderColor: "#f0691b",
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    scales: {
+      yAxes: [{ ticks: { min: 6, max: 16 } }],
+    },
+  },
+});
+
+var cards = document.querySelectorAll(".card");
+
+[...cards].forEach((card) => {
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
+  });
+});
+
+function refrestTime() {
+  var dt = new Date();
+  document.getElementById("date-time").innerHTML = dt;
+}
+setInterval(refrestTime, 1000);
